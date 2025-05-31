98736634959e2e23ae1cb527b196ea544b64a5c1
S4PPH1R3 <lytasia@outlook.fr>
2025-05-31 16:32:27 +0200

    Added Mute Icon

--- src/components/icons/IconVolume.vue
+++ src/components/icons/IconVolume.vue
@@ -1,5 +1,11 @@
+<script setup>
+const props = defineProps({
+  "muted": Boolean,
+})
+</script>
 <template>
   <svg viewBox="0 0 24 24" width="24" height="24" class="volume-icon">
     <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z" style="fill: white"></path>
+    <line x1="1" y1="1" x2="23" y2="23" style="stroke: white; stroke-width: 2" v-if="muted"></line>
   </svg>
 </template>
